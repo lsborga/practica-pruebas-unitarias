edc5b5c66bd3da64e1b843edcb67c3424348fa83
Lucho <lucho.s.b@hotmail.com>
2025-03-07 00:56:43 -0500

    Commit 2

--- test_nombres_animales.py
+++ test_nombres_animales.py
@@ -1,6 +1,10 @@
 
+import allure
 from animales import obtener_nombres_animales
 
+@allure.description("Test donde se devuelve una lista de nombres de animales")
+@allure.feature("Animales")
+@allure.epic("Listas de nombres")
 def test_obtener_nombres():
     nombres = obtener_nombres_animales()
     assert isinstance(nombres, list)
