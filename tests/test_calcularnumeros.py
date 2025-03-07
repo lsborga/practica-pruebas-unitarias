c2c85b1587f37ac384bd8d0ffa2cbbdf7c7b5bd9
Lucho <lucho.s.b@hotmail.com>
2025-03-07 01:28:41 -0500

    Final before Azure

--- tests/test_calcularnumeros.py
+++ tests/test_calcularnumeros.py
@@ -3,7 +3,7 @@ from calcular_numeros import calcular_promedio
 
 @allure.description("Test donde se calcula el promedio")
 @allure.feature("Promedio")
-@allure.epic("Listas de nombres")
+@allure.epic("Promedio")
 def test_calcularpromedio():
     numeros = [1,2,3,4,5]
     resultado = calcular_promedio(numeros)
